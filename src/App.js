a8e2d804d9650aa8809a4a38dd574588e4af9c3e
Dima <vuyko@ukr.net>
2019-10-20 22:50:54 +0400

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
