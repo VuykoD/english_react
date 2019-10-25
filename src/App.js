7369e8d9260f891efb934631aae4f3c06c2e9231
Dima <vuyko@ukr.net>
2019-10-25 21:02:29 +0400

    new commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux'
 import { Button } from 'react-bootstrap';
+import  get  from 'lodash/get';
 import Header from './src-core/components/header/header.jsx';
 
 import './App.css';
 
-export default class App extends Component {
+const content = {
+    body: {
+        ru: 'Тело',
+        ukr: 'Тіло',
+    }
+};
+
+class App extends Component {
+
 
   render(){
+    const {store = {}, onChangeSiteLang, onChangeLearnedLang} = this.props;
+    const {siteLang} = store;
+    const body = get(content, `body[${siteLang}]`);
+
     return (
       <Fragment>
-        <Header />
-        <Button variant="primary">Тело</Button>
+        <Header
+            store={store}
+            onChangeSiteLang={onChangeSiteLang}
+            onChangeLearnedLang={onChangeLearnedLang}
+        />
+        <Button variant="primary">{body}</Button>
       </Fragment>
     );
   }
-};
+}
+
+export default connect(
+    state => ({
+        store: state
+    }),
+    dispatch => ({
+        onChangeSiteLang: (lang) => {
+            dispatch({ type: 'SITE_LANG', payload: lang })
+        },
+        onChangeLearnedLang: (lang) => {
+            dispatch({ type: 'LEARNED_LANG', payload: lang })
+        },
+    })
+)(App);
