8fa226fd5d3dd004493d8b61eba1fe59a9db9942
Vlad <VuykoVlad@ukr.net>
2019-11-23 00:31:16 +0200

    front

--- src/src-pages/murphy-page/Murphy1.jsx
+++ src/src-pages/murphy-page/Murphy1.jsx
@@ -3,15 +3,17 @@ import map from 'lodash/map';
 import {Col, Container, Row, ListGroup} from "react-bootstrap";
 import  murphy1Item from "../../../src/dict/murphy1Item"
 
+import '../../scc/course.css';
+
 export default class Course extends Component {
     render() {
         return (
             <Container>
                 <Row>
-                    <Col sm={2}>
-                        <div><img src="" alt=""/></div>
+                    <Col sm={1}>
+                        <div><img src="images/paint.png" alt=""className="paint-left"/></div>
                     </Col>
-                    <Col sm={8}>
+                    <Col sm={10}>
                         <h1>Murphy 1</h1>
                         <ListGroup>
                             {map(murphy1Item, (item, key) => {
@@ -28,7 +30,9 @@ export default class Course extends Component {
                             })}
                         </ListGroup>
                     </Col>
-                    <Col sm={2}/>
+                    <Col sm={1}>
+                        <div><img src="images/paint.png" alt=""className="paint-right"/></div>
+                    </Col>
                 </Row>
             </Container>
         );
--- src/src-pages/murphy-page/m1units/Unit1.jsx
+++ src/src-pages/murphy-page/m1units/Unit1.jsx
@@ -5,6 +5,7 @@ import map from 'lodash/map';
 import findIndex from 'lodash/findIndex';
 
 import '../../../scc/unit.css';
+import '../../../scc/course.css'
 import FormControl from "react-bootstrap/FormControl";
 
 const content = {
@@ -104,90 +105,93 @@ export default class Course extends Component {
         const {courseItems} = this.state;
         return (
             <Container>
-                <Row className="text-center">
-                    <Col sm={3}/>
-                    <Col sm={6}>
-                        <h1>Unit 1: Present continuous (I'm doing)</h1>
+                <Row>
+                    <Col sm={1}>
+                        <div><img src="images/paint.png" alt="" className="paint-left"/></div>
                     </Col>
-                    <Col sm={3}/>
-                </Row>
-                <Row className="text-center learning">
-                    <Col sm={3}>
-                        <div><img src="" alt=""/></div>
-                    </Col>
-                    <Col sm={6}>
-                        <Button variant="primary">{learning}</Button>
-                    </Col>
-                    <Col sm={3}/>
-                </Row>
-                <ListGroup>
-                    {map(courseItems, (item, key) => {
-                        const odd = key & 1 ? 'light' : 'primary';
-                        return (
-                            <ListGroup.Item variant={odd} key={item.id}>
+                    <Col sm="10">
+                        <Row className="text-center learning">
+                            <Col sm={2}/>
+                            <Col sm={8}>
+                                <h1>Unit 1: Present continuous (I'm doing)</h1>
+
+                                <Button variant="primary">{learning}</Button>
+                            </Col>
+                            <Col sm={2}/>
+                        </Row>
+                        <ListGroup>
+                            {map(courseItems, (item, key) => {
+                                const odd = key & 1 ? 'light' : 'primary';
+                                return (
+                                    <ListGroup.Item variant={odd} key={item.id}>
+                                        <Row className="unit-row">
+                                            <Col sm="1" className="display-none">
+                                                <FormControl type="text" defaultValue={item.id} disabled
+                                                             id={`row${key + 1}_id`}/>
+                                            </Col>
+                                            <Col>
+                                                <FormControl type="text" className="mr-sm-2" id={`row${key + 1}_eng`}
+                                                             defaultValue={item.eng}/>
+                                            </Col>
+                                            <Col>
+                                                <FormControl type="text" className="mr-sm-2" id={`row${key + 1}_transl`}
+                                                             defaultValue={item.transl}/>
+                                            </Col>
+                                            <Col sm="1">
+                                                <Button
+                                                    row={key + 1}
+                                                    className="buttons"
+                                                    variant="primary"
+                                                    onClick={this.save}
+                                                >
+                                                    save
+                                                </Button>
+                                            </Col>
+                                            <Col sm="1">
+                                                <Button
+                                                    row={key + 1}
+                                                    className="buttons"
+                                                    variant="dark"
+                                                    onClick={this.delete}
+                                                >
+                                                    Delete
+                                                </Button>
+                                            </Col>
+                                        </Row>
+
+                                    </ListGroup.Item>
+                                )
+                            })}
+                            <ListGroup.Item variant="light">
                                 <Row className="unit-row">
                                     <Col sm="1" className="display-none">
-                                        <FormControl type="text" defaultValue={item.id} disabled
-                                                     id={`row${key + 1}_id`}/>
+                                        <FormControl type="text" disabled id='row_new_id' className="display-none"/>
                                     </Col>
                                     <Col>
-                                        <FormControl type="text" className="mr-sm-2" id={`row${key + 1}_eng`}
-                                                     defaultValue={item.eng}/>
+                                        <FormControl type="text" className="mr-sm-2" id={`row_new_eng`}/>
                                     </Col>
                                     <Col>
-                                        <FormControl type="text" className="mr-sm-2" id={`row${key + 1}_transl`}
-                                                     defaultValue={item.transl}/>
+                                        <FormControl type="text" className="mr-sm-2" id={`row_new_transl`}/>
                                     </Col>
                                     <Col sm="1">
                                         <Button
-                                            row={key + 1}
-                                            className="buttons"
                                             variant="primary"
+                                            className="buttons"
                                             onClick={this.save}
+                                            row="_new"
                                         >
                                             save
                                         </Button>
                                     </Col>
-                                    <Col sm="1">
-                                        <Button
-                                            row={key + 1}
-                                            className="buttons"
-                                            variant="dark"
-                                            onClick={this.delete}
-                                        >
-                                            Delete
-                                        </Button>
-                                    </Col>
+                                    <Col sm="1"/>
                                 </Row>
-
                             </ListGroup.Item>
-                        )
-                    })}
-                    <ListGroup.Item variant="light">
-                        <Row className="unit-row">
-                            <Col sm="1" className="display-none">
-                                <FormControl type="text" disabled id='row_new_id' className="display-none"/>
-                            </Col>
-                            <Col>
-                                <FormControl type="text" className="mr-sm-2" id={`row_new_eng`}/>
-                            </Col>
-                            <Col>
-                                <FormControl type="text" className="mr-sm-2" id={`row_new_transl`}/>
-                            </Col>
-                            <Col sm="1">
-                                <Button
-                                    variant="primary"
-                                    className="buttons"
-                                    onClick={this.save}
-                                    row="_new"
-                                >
-                                    save
-                                </Button>
-                            </Col>
-                            <Col sm="1"/>
-                        </Row>
-                    </ListGroup.Item>
-                </ListGroup>
+                        </ListGroup>
+                    </Col>
+                    <Col sm={1}>
+                        <div><img src="images/paint.png" alt="" className="paint-right"/></div>
+                    </Col>
+                </Row>
             </Container>
         )
     }
