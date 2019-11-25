2621b2d884a664b33da2487a223bc6d3f668171d
Vlad <VuykoVlad@ukr.net>
2019-11-25 04:07:22 +0200

    Updates

---
+++ precache-manifest.25b01febbf7062537f483eb4762b2102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cc3ebfbd9e16083dacf5e5c159e20c",
+    "url": "/english_react/index.html"
+  },
+  {
+    "revision": "4052d93756f1c05a9a4c",
+    "url": "/english_react/static/css/2.1b8295f7.chunk.css"
+  },
+  {
+    "revision": "62beb0276d15e9e4f47c",
+    "url": "/english_react/static/css/main.fd4ef212.chunk.css"
+  },
+  {
+    "revision": "4052d93756f1c05a9a4c",
+    "url": "/english_react/static/js/2.420b7e8f.chunk.js"
+  },
+  {
+    "revision": "62beb0276d15e9e4f47c",
+    "url": "/english_react/static/js/main.60c2b312.chunk.js"
+  },
+  {
+    "revision": "6080283de34e18a085c2",
+    "url": "/english_react/static/js/runtime-main.cf643162.js"
+  },
+  {
+    "revision": "315a454493d6a52d080a4951352a28b7",
+    "url": "/english_react/static/media/bob-van-aubel-ray-bans.315a4544.jpg"
+  }
+]);
