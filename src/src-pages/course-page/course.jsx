e33994ac64f63fa255f44e54e8283550b149064d
VuykoD <vuyko@ukr.net>
2022-12-20 11:05:08 +0100

    fix

--- src/src-pages/course-page/course.jsx
+++ src/src-pages/course-page/course.jsx
@@ -1,15 +1,61 @@
 import React, {Component, Fragment} from 'react';
 import map from 'lodash/map';
+import findIndex from "lodash/findIndex";
+import filter from "lodash/filter";
 import {Col, Container, Row, Accordion, Card, Form} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import courseUnits from "../../dict/courseUnits";
 import courseNames from "../../dict/courseNames";
+import courseItems from "../../dict/courseItems";
 
 import '../../scc/course.css';
 
 export default class Course extends Component {
+    constructor(props) {
+        super(props);
+        this.localProgress = localStorage.progress ? JSON.parse(localStorage.progress) : [];
+        const selectedCourses = [];
+        map(this.localProgress, (itProgress) =>{
+            const unitIndex = findIndex(courseItems, {'id': itProgress.entity_id });
+            if (unitIndex !== -1) {
+                const unitId = courseItems[unitIndex].unitId;
+                const selectedCoursesIndex = findIndex(selectedCourses, it => it === unitId);
+
+                if(selectedCoursesIndex === -1) selectedCourses.push(unitId);
+            }
+        });
+
+        this.state = {
+            selectedCourses
+        };
+    }
+
+    onChangeCheck = (unitId) => {
+        const { selectedCourses } = this.state;
+        const selectedCoursesIndex = findIndex(selectedCourses, course => course === unitId);
+        if (selectedCoursesIndex === -1) {
+            this.setState({selectedCourses: [...selectedCourses, unitId]});
+        } else {
+            const courses = [];
+            map(selectedCourses, course => {
+                if (course !== unitId) courses.push(course)
+            })
+            this.setState({selectedCourses: courses});
+        }
+    };
+
     render() {
-        const checked=false;
+        const { selectedCourses } = this.state;
+        let progress = [];
+        map(selectedCourses, unitId => {
+            const items = filter(courseItems, {'unitId': unitId });
+            progress = [...progress, ...items];
+        });
+        const localProgress = [];
+        map(progress, it => {
+            localProgress.push({"entity_id": it.id})
+        });
+        localStorage.progress = JSON.stringify(localProgress);
 
         return (
             <Container>
@@ -27,9 +73,18 @@ export default class Course extends Component {
                                     </Accordion.Toggle>
                                     {map(courseUnits, (item, key) => {
                                         if (item.courseId !== course.id) return null;
+
+                                        let checked=false;
+                                        map(selectedCourses, (course) => {
+                                            if (course === item.id) return checked = true;
+                                        });
                                         return (
                                             <div key={key} className="main">
-                                                <Form.Check type="checkbox"/>
+                                                <Form.Check
+                                                    type="checkbox"
+                                                    onChange={() => this.onChangeCheck(item.id)}
+                                                    checked={checked}
+                                                />
                                                 <Link to={`/english_react/course${item.url}`}>
                                                     <Card.Body children={item.name}/>
                                                 </Link>
