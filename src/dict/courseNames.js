ca496643a7b7fbf2dcca4231c139674153a52c65
Dmytro Vuiko <dmvu@boozt.com>
2023-06-05 08:41:48 +0200

    fix translation

--- src/dict/courseNames.js
+++ src/dict/courseNames.js
@@ -9,19 +9,19 @@ const courseNames = [
         name: 'Common phrases',
         active: true,
     },
-    {
-        id: 1,
-        name: 'Murphy blue',
-        active: true,
-    },
-    {
-        id: 2,
-        name: 'Murphy 2',
-        active: true,
-    },
+    // {
+    //     id: 1,
+    //     name: 'Murphy blue',
+    //     active: true,
+    // },
+    // {
+    //     id: 2,
+    //     name: 'Murphy 2',
+    //     active: true,
+    // },
     {
         id: 3,
-        name: 'School 8 class',
+        name: 'Polish school 8 class',
         active: true,
     },
 ];
